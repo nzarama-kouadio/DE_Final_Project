26d643db65ab74c4e62d9d42667ff4d18d0221d4
skyeau9 <skye.augsorn@duke.edu>
2024-12-12 12:56:19 -0800

    fix typo

--- api/explanation_service.py
+++ api/explanation_service.py
@@ -12,7 +12,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_DEFAULT_REGION"),
+    region_name="us-east-2",
 )
 
 @lru_cache(maxsize=100)
